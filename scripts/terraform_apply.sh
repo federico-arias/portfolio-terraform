7602e1e09f25023523c4f1a4d441417df38c5732
Federico Arias <federicoariasr@gmail.com>
2022-04-22 00:49:15 +0200

    merge

---
+++ scripts/terraform_apply.sh
@@ -0,0 +1,27 @@
+terraform refresh
+
+account_id=$(terraform output -raw account_id)
+region=$(terraform output -raw region)
+project=$(terraform output -raw project)
+backend_tag=$(terraform output -raw backend_tag)
+frontend_tag=$(terraform output -raw frontend_tag)
+aws_profile=$(terraform output -raw aws_profile)
+
+
+# TODO: move this to a CI/CD, secrets come from env vars, tags from git tags.
+apply() {
+  # get secrets from secretmanager
+  db_password=$(aws secretsmanager \
+    get-secret-value \
+    --profile ${aws_profile} \
+    --region ${region} \
+    --secret-id "${project}-db_password" | jq --raw-output '.SecretString')
+
+  terraform apply \
+    -var="project=${project}" \
+    -var="db_password=${db_password}" \
+    -var="backend_tag=${backend_tag}" \
+    -var="frontend_tag=${frontend_tag}"
+}
+
+apply
